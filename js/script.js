460d1f69bcbe6b64809a0d6a39d19e691d131f48
Angelika Halicka <87149352+angiihal@users.noreply.github.com>
2022-01-23 22:52:40 +0000

    create function to count remaining guesses, add async function for random letters

--- js/script.js
+++ js/script.js
@@ -15,28 +15,38 @@ const messages = document.querySelector(".message");
 //hidden button that will appear promting the player to play again
 const playAgainButton = document.querySelector(".play-again");
 
-const word = "magnolia";
+let word = "magnolia";
 const guessedLetterArray = [];
+let guessesRemaining = 8;
+
+const getWord = async function () {
+    const res = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+    const data = await res.text();
+    const wordArray = word.split("\n");
+    const randomWord = Math.floor(Math.random() * wordArray.length);
+    word = wordArray[randomWord].trim();
+    placeholder(word);
+};
+
+getWord();
 
 const placeholder = function (word) {
     const placeholderLetters = [];
     for (const letter of word) {
-        console.log(letter);
+        //console.log(letter);
         placeholderLetters.push("●");
     }
     wordInProgress.innerText = placeholderLetters.join("");
 };
 
-placeholder(word);
-
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
     messages.innerText = "";
-    const letter = textInput.value;
-    const inputResult = validateInput(letter);
+    const guess = textInput.value;
+    const inputResult = validateInput(guess);
     
     if (inputResult) {
-        makeGuess(letter);
+        makeGuess(guess);
     }
     textInput.value = "";
 });
@@ -55,13 +65,14 @@ const validateInput = function (input) {
     }
 };
 
-const makeGuess = function (letter) {
-    letter = letter.toUpperCase();
-    if (guessedLetterArray.includes(letter)) {
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetterArray.includes(guess)) {
         messages.innerText = "You have already guessed this letter. Try again";
     } else {
-        guessedLetterArray.push(letter);
+        guessedLetterArray.push(guess);
         console.log(guessedLetterArray);
+        countGuesses(guess);
         displayLetters();
         updateWord(guessedLetterArray);
     }
@@ -69,9 +80,9 @@ const makeGuess = function (letter) {
 
 const displayLetters = function () {
     guessedLetters.innerHTML = "";
-    for (const letters of guessedLetterArray) {
+    for (const letter of guessedLetterArray) {
         const li = document.createElement("li");
-        li.innerText = letters;
+        li.innerText = letter;
         guessedLetters.append(li);
     }
 };
@@ -80,15 +91,33 @@ const updateWord = function (guessedLetterArray) {
     const wordUpper = word.toUpperCase();
     const wordArray = wordUpper.split("");
     const revealLetter = [];
-    for (const letters of wordArray) {
-        if (guessedLetterArray.includes(letters)) {
-            revealLetter.push(letters.toUpperCase());
+    for (const letter of wordArray) {
+        if (guessedLetterArray.includes(letter)) {
+            revealLetter.push(letter.toUpperCase());
         } else {
             revealLetter.push("●");
         }
     }
     wordInProgress.innerText = revealLetter.join("");
     successfulGuess();
+};
+
+const countGuesses = function (guess) {
+    const uppercase = word.toUpperCase();
+    if (!uppercase.includes(guess)) {
+        messages.innerText = `The word doesn't contain ${guess}.`;
+        guessesRemaining -= 1;
+    } else {
+        messages.innerText = `The letter ${guess} is in the word`;
+    }
+
+    if (remainingGuesses === 0) {
+        messages.innerHTML = `The game is over. The word was <span class="highlight">${word}</span>.`;
+    } else if (remainingGuesses === 1) {
+        remainingGuessesSpan.innerText = `${guessesRemaining} guess`;
+    } else {
+        remainingGuessesSpan.innerText = `${guessesRemaining} guesses`;
+    }
 };
 
 const successfulGuess = function () {
